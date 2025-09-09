b30d08e689dd8acc4952b840885fe1d39c565582
mathistrunet <mathtrunet101@gmail.com>
2025-09-09 14:06:54 +0200

    Create geometry.js

---
+++ src/utils/geometry.js
@@ -0,0 +1,23 @@
+import { toLambert93 } from "./proj";
+
+// Convertis les sommets du polygone dessiné en WGS en Lambert93
+export function ringToGml(ringLonLat) {
+  const xy = ringLonLat.map(([lon, lat]) => {
+    const [x, y] = toLambert93([lon, lat]);
+    return `${x.toFixed(3)},${y.toFixed(3)}`;
+  });
+  if (xy.length && xy[0] !== xy[xy.length - 1]) xy.push(xy[0]);
+  return xy.join(" ");
+}
+
+// Calcule avec la formule du lacet l'aire du polygone
+export function ringAreaM2(ringLonLat) {
+  const pts = ringLonLat.map(([lon, lat]) => toLambert93([lon, lat]));
+  let sum = 0;
+  for (let i = 0; i < pts.length - 1; i++) {
+    const [x1, y1] = pts[i];
+    const [x2, y2] = pts[i + 1];
+    sum += x1 * y2 - x2 * y1;
+  }
+  return Math.abs(sum) / 2;
+}
