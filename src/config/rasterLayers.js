31690a600f9ac157fd67520aabdf28f95a6fb17c
mathistrunet <mathtrunet101@gmail.com>
2025-09-09 14:05:40 +0200

    Create rasterLayers.js

---
+++ src/config/rasterLayers.js
@@ -0,0 +1,33 @@
+export const RASTER_LAYERS = [
+  {
+    id: "osm_std",
+    label: "Fond OpenStreetMap",
+    url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    subdomains: null,
+    tileSize: 256,
+    attribution: "© OpenStreetMap",
+    defaultVisible: true,
+    defaultOpacity: 1.0,
+  },
+  {
+    id: "opentopo",
+    label: "OpenTopoMap",
+    url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    subdomains: ["a", "b", "c"],
+    tileSize: 256,
+    attribution: "© OpenTopoMap",
+    defaultVisible: false,
+    defaultOpacity: 0.8,
+  },
+
+  {
+    id: "ign_satellite",
+    label: "IGN Satellite",
+    url: "https://data.geopf.fr/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&STYLE=normal&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&FORMAT=image/jpeg",
+    subdomains: null,
+    tileSize: 256,
+    attribution: "© IGN",
+    defaultVisible: false,
+    defaultOpacity: 1.0,
+  },
+];
