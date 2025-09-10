590f483d6e63f461ba845aca1ccc87950cbf7970
mathistrunet <mathtrunet101@gmail.com>
2025-09-10 13:59:30 +0200

    Add toolbar toggle for multi-parcel selection

--- src/features/map/useMapInitialization.js
+++ src/features/map/useMapInitialization.js
@@ -3,6 +3,7 @@ import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import MultipleSelectionMode from "./multipleSelectionMode.js";
 import { useRasterLayers } from "./useRasterLayers";
 
 if (typeof window !== "undefined") window.mapboxgl = maplibregl;
@@ -71,6 +72,7 @@ export function useMapInitialization() {
         displayControlsDefault: false,
         controls: {},
         defaultMode: "simple_select",
+        modes: { ...MapboxDraw.modes, multiple_selection: MultipleSelectionMode },
         styles: [
           {
             id: "draw-polygon-fill-inactive",
